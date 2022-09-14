b26da96a7ea2ac4deac1c076e07f1f559b2a41a7
sheela7 <sheela7>
2022-09-14 14:12:04 +0545

    error handling part completed

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -6,6 +6,7 @@ const WorkoutForm = () => {
    const [load, setLoad]= useState('')
    const [reps, setReps]= useState('')
    const [error, setError]= useState(null)
+   const [emptyFields, setEmptyFields] = useState([])
     
    const handleSubmit = async(e) =>{e.preventDefault()
 const workout = {title, load, reps}
@@ -13,12 +14,14 @@ const response = await fetch('/api/workouts',{method: 'POST', body:JSON.stringif
 const json = await response.json()
 if(!response.ok){
 setError(json.error)
+setEmptyFields(json.emptyFields)
 }
 if(response.ok){
     setTitle('')
     setLoad('')
     setReps('')
     setError(null)
+    setEmptyFields([])
     console.log('new workout added',json)
     dispatch({type: 'CREATE_WORKOUT', payload:json})
 }
@@ -28,14 +31,14 @@ if(response.ok){
         <form className="create" onSubmit={handleSubmit}>
             <h3>Add a New Workout</h3>
             <label>Exercise Title:</label>
-            <input type="text" onChange={(e) =>setTitle(e.target.value)} value={title}/>
+            <input type="text" onChange={(e) =>setTitle(e.target.value)} value={title} className={emptyFields.includes('title') ? 'error': ''}/>
 
             <label>Load (in Kg):</label>
-            <input type="number" onChange={(e) =>setLoad(e.target.value)} value={load}/>
+            <input type="number" onChange={(e) =>setLoad(e.target.value)} value={load} className={emptyFields.includes('load') ? 'error': ''}/>
 
 
             <label>Reps:</label>
-            <input type="number" onChange={(e) =>setReps(e.target.value)} value={reps}/>
+            <input type="number" onChange={(e) =>setReps(e.target.value)} value={reps} className={emptyFields.includes('reps') ? 'error': ''}/>
 
             <button>Add Workout</button>
             {error && <div className="error">{error}</div>}
