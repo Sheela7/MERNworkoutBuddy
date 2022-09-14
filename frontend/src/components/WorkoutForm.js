f7bde401e6ee15af7b477f9d912dbf6e876b2e7b
sheela7 <sheela7>
2022-09-14 12:31:18 +0545

    added react context

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -1,5 +1,7 @@
 import { useState } from "react"
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext';
 const WorkoutForm = () => {
+    const {dispatch} = useWorkoutsContext()
    const [title, setTitle]= useState('')
    const [load, setLoad]= useState('')
    const [reps, setReps]= useState('')
@@ -18,6 +20,7 @@ if(response.ok){
     setReps('')
     setError(null)
     console.log('new workout added',json)
+    dispatch({type: 'CREATE_WORKOUT', payload:json})
 }
 }
    
